2ae388db900eaebbdab528ba5046f45ff559a1cc
Ali <ali@deepcrawl.com>
2020-04-17 18:43:01 +0100

    dep

--- src/infrastructure/deployment/deployment.stack.ts
+++ src/infrastructure/deployment/deployment.stack.ts
@@ -1,1 +1,17 @@
+import { EventAction, FilterGroup, Project, Source } from "@aws-cdk/aws-codebuild";
+import { Construct, Stack, StackProps } from "@aws-cdk/core";
 
+const source = Source.gitHub({
+  owner: "ali-habibzadeh",
+  repo: "alexa",
+  webhook: true,
+  webhookFilters: [FilterGroup.inEventOf(EventAction.PUSH).andBranchIs("master")],
+});
+
+export class DeploymentStack extends Stack {
+  constructor(parent: Construct, id: string, props?: StackProps) {
+    super(parent, id, props);
+  }
+
+  public project = new Project(this, "my-deployment-stacl", {});
+}
