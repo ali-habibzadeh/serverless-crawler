8953901f89f278f558b98ab7c2cc24220ad82591
Ali <ali@deepcrawl.com>
2020-04-24 13:25:26 +0100

    test

--- src/handlers/speech/speech.handler.integration.ts
+++ src/handlers/speech/speech.handler.integration.ts
@@ -1,5 +1,3 @@
-import { ConfigService } from "../../config/config.service";
-import { S3Service } from "../../core/s3/s3.service";
 import { SpeechHandler } from "./speech.handler";
 
 jest.setTimeout(30000);
@@ -12,11 +10,6 @@ describe("scope ", () => {
   it("works with async", async () => {
     const output = await handler.respond();
     const synthesisTask = output.SynthesisTask;
-    const s3 = S3Service.getInstance();
-    const bucket = ConfigService.getInstance().environment.speechBucketName;
-    const key = `${synthesisTask?.TaskId}.${synthesisTask?.OutputFormat}`;
-    console.log(key);
-    await s3.getObject({ Bucket: bucket, Key: key }).promise();
-    expect(1).toBe(1);
+    expect(synthesisTask?.OutputFormat).toBe("mp3");
   });
 });
