6bf792da9fb6b9ce1e7f878563ecdb5554b5df95
Ali <ali@deepcrawl.com>
2020-04-30 10:42:19 +0100

    handler

--- src/crawler/start-crawl.handler.ts
+++ src/crawler/start-crawl.handler.ts
@@ -3,20 +3,29 @@ import "reflect-metadata";
 import { DynamoDBRecord, DynamoDBStreamEvent } from "aws-lambda";
 
 import { DynamodbService } from "../core/dynamodb/dynamodb.service";
+import { CrawlUrl } from "./url-processing/crawl-url.model";
 
 export class StartCrawlHandler {
+  private inserts = this.event.Records.filter((record) => record.eventName === "INSERT");
+  private converter = DynamodbService.Converter;
+
   constructor(protected event: DynamoDBStreamEvent) {}
 
   public async handle(): Promise<string> {
-    await Promise.all(this.event.Records.map(async (record) => this.processUrl(record)));
+    await Promise.all(this.inserts.map(async (record) => this.processUrl(record)));
     return "done.";
   }
 
-  // tslint:disable-next-line: no-feature-envy
-  private async processUrl(record: DynamoDBRecord): Promise<void> {
-    const ni = record.dynamodb!.NewImage;
-    if (ni) {
-      console.log(DynamodbService.Converter.unmarshall(ni));
+  private async processUrl(record: DynamoDBRecord): Promise<any> {
+    const url = this.getUrl(record);
+    console.log("model extracted", url);
+  }
+
+  private getUrl(record: DynamoDBRecord): CrawlUrl {
+    const { NewImage } = record.dynamodb!;
+    if (NewImage) {
+      return <CrawlUrl>this.converter.unmarshall(NewImage);
     }
+    throw new Error(`Invalid DynamoDBRecord ${record}`);
   }
 }
