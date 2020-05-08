3639ea4d8133fc404d743d237fdf22dd55d01dc5
Ali <ali@deepcrawl.com>
2020-05-08 16:38:03 +0100

    start handler

--- src/crawling/start-crawl.handler.ts
+++ src/crawling/start-crawl.handler.ts
@@ -1,9 +1,22 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { plainToClass } from "class-transformer";
+
+import { CrawlUrl } from "./url-processing/crawl-url.model";
+import { UrlsProcessor } from "./url-processing/url-processor";
 
 export class StartCrawlHandler {
   constructor(private event: APIGatewayProxyEvent) {}
 
-  public async handle(): Promise<string | null> {
-    return this.event.body;
+  public async handle(): Promise<string> {
+    await new UrlsProcessor(this.getUrl()).process();
+    return "started";
+  }
+
+  private getUrl(): CrawlUrl {
+    if (this.event.body) {
+      const { url } = JSON.parse(this.event.body);
+      return plainToClass(CrawlUrl, { url });
+    }
+    throw new Error(`Invalid body: ${this.event.body}`);
   }
 }
