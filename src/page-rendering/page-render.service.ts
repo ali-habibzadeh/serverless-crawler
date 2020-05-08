2eb5c5491f8a2dc8d9acc43060df6cd4f6a1b795
Ali <ali@deepcrawl.com>
2020-05-08 20:24:23 +0100

    close

--- src/page-rendering/page-render.service.ts
+++ src/page-rendering/page-render.service.ts
@@ -1,11 +1,10 @@
-import { Browser, Page, Response } from "puppeteer-core";
+import { Page, Response } from "puppeteer-core";
 
 import { MetricNames, metricsContainers as metrics } from "../metrics/metrics-list";
 import { BrowserService } from "./config/constants/launch-options";
 import { PageRequestHandler } from "./config/page-request.handler";
 
 export class PageRenderService {
-  private browser!: Browser;
   private page!: Page;
 
   constructor(private url: string) {}
@@ -13,13 +12,13 @@ export class PageRenderService {
   public async getPageRenderMetrics(): Promise<Record<MetricNames, any>> {
     const response = await this.getResponse();
     const results = await Promise.all(metrics.map((metric) => new metric(this.page, response).getMetrics()));
-    await this.close();
+    await BrowserService.browser.close();
     return results.flat(1).reduce((obj, metric) => ({ ...obj, ...metric }));
   }
 
   private async getResponse(): Promise<Response | null> {
-    this.browser = await BrowserService.getBrowser();
-    this.page = await this.browser.newPage();
+    await BrowserService.getBrowser();
+    this.page = await BrowserService.browser.newPage();
     await this.setPageHandlers();
     return this.page.goto(this.url);
   }
@@ -27,9 +26,5 @@ export class PageRenderService {
   private async setPageHandlers(): Promise<void> {
     await this.page.setRequestInterception(true);
     this.page.on("request", async (request) => new PageRequestHandler(request).handle());
-  }
-
-  private async close(): Promise<void> {
-    this.browser.disconnect();
   }
 }
