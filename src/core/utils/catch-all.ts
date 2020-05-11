f56bd0f90cb72e41248ccdbe09c225112db3822f
Ali <ali@deepcrawl.com>
2020-05-11 21:18:29 +0100

    show context

--- src/core/utils/catch-all.ts
+++ src/core/utils/catch-all.ts
@@ -1,4 +1,4 @@
 import Catch from "catch-decorator";
 
 // tslint:disable-next-line: variable-name
-export const CatchAll = Catch(Error, (err) => console.log("APPLICATION_ERROR", err.message));
+export const CatchAll = Catch(Error, (err, ctx) => console.log("APPLICATION_ERROR", err.message, ctx));
