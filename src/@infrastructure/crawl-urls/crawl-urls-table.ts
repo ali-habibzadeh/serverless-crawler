3de523c36fd493e4908ab6b25140de7d613825d9
Ali <ali@deepcrawl.com>
2020-05-11 23:47:20 +0100

    this.deadLetterQueue

--- src/@infrastructure/crawl-urls/crawl-urls-table.ts
+++ src/@infrastructure/crawl-urls/crawl-urls-table.ts
@@ -1,16 +1,20 @@
 import { AttributeType, StreamViewType, Table } from "@aws-cdk/aws-dynamodb";
 import { StartingPosition } from "@aws-cdk/aws-lambda";
-import { DynamoEventSource } from "@aws-cdk/aws-lambda-event-sources";
+import { DynamoEventSource, SqsDlq } from "@aws-cdk/aws-lambda-event-sources";
+import { Queue } from "@aws-cdk/aws-sqs";
 import { Construct, Duration } from "@aws-cdk/core";
 
 export class CrawlUrlsTable extends Construct {
   constructor(parent: Construct, id: string) {
     super(parent, id);
   }
+
   public table = new Table(this, "crawlUrlsTable", {
     partitionKey: { name: "url", type: AttributeType.STRING },
     stream: StreamViewType.NEW_IMAGE,
   });
+
+  public deadLetterQueue = new Queue(this, "deadLetterQueue");
 
   public eventSource = new DynamoEventSource(this.table, {
     startingPosition: StartingPosition.TRIM_HORIZON,
@@ -19,5 +23,6 @@ export class CrawlUrlsTable extends Construct {
     retryAttempts: 10,
     batchSize: 5,
     bisectBatchOnError: true,
+    onFailure: new SqsDlq(this.deadLetterQueue),
   });
 }
