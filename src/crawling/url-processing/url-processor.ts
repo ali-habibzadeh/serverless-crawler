0bb4dda0ae1858b4ed8eb1b99428fdb4e9773517
Ali <ali@deepcrawl.com>
2020-05-14 21:33:35 +0100

    check for old image

--- src/crawling/url-processing/url-processor.ts
+++ src/crawling/url-processing/url-processor.ts
@@ -1,3 +1,4 @@
+import { DynamoDBRecord } from "aws-lambda";
 import { plainToClass } from "class-transformer";
 
 import { DynamodbService } from "../../core/dynamodb/dynamodb.service";
@@ -8,7 +9,11 @@ import { CrawlUrl } from "./crawl-url.model";
 
 export class UrlsProcessor {
   private dynamodb = DynamodbService.getInstance();
-  constructor(private crawlUrl: CrawlUrl) {}
+  private converter = DynamodbService.Converter;
+  private alreadyModified = this.record.dynamodb?.OldImage !== undefined;
+  private crawlUrl = this.getUrl();
+
+  constructor(private record: DynamoDBRecord) {}
 
   public async process(): Promise<void> {
     const renderer = new PageRenderService(this.crawlUrl.url);
@@ -19,10 +24,22 @@ export class UrlsProcessor {
 
   private async crawlNextBatch(links: string[]): Promise<void> {
     const level = this.crawlUrl.level + 1;
-    const toSave = links.map((url) => plainToClass(CrawlUrl, { url, level }));
+    const toSave = links.map((url) =>
+      plainToClass(CrawlUrl, { url, ...(!this.alreadyModified && { level }) })
+    );
     const writer = this.dynamodb.batchPut(toSave);
     for await (const persisted of writer) {
       console.log(`Successfully wrote ${persisted.url}`);
     }
+  }
+
+  private getUrl(): CrawlUrl {
+    const { NewImage, OldImage } = this.record.dynamodb!;
+    const image = OldImage || NewImage;
+    if (image) {
+      const unmarshalled = <CrawlUrl>this.converter.unmarshall(image);
+      return plainToClass(CrawlUrl, unmarshalled);
+    }
+    throw new Error(`Invalid DynamoDBRecord ${this.record}`);
   }
 }
