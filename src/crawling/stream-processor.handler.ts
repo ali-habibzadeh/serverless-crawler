4f1987147036aae0ba1509fdd77f0bf9b122d757
Ali <ali@deepcrawl.com>
2020-05-14 22:07:56 +0100

    inserts

--- src/crawling/stream-processor.handler.ts
+++ src/crawling/stream-processor.handler.ts
@@ -18,7 +18,10 @@ export class StreamProcessorHandler {
   public async handle(): Promise<string> {
     await BrowserService.createBrowser();
     const inserts = this.event.Records.filter((record) => record.eventName === "INSERT");
-    console.log("INSERTS", inserts);
+    console.log(
+      "INSERTS",
+      inserts.map((insert) => insert.dynamodb?.NewImage)
+    );
     await Promise.all(inserts.map(async (record) => this.processUrl(record)));
     await BrowserService.close();
     return `done.`;
