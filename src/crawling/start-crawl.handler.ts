2c2db347458f09c36d80dbecc459a12c8d4821cf
Ali <ali@deepcrawl.com>
2020-05-15 15:29:51 +0100

    level 0

--- src/crawling/start-crawl.handler.ts
+++ src/crawling/start-crawl.handler.ts
@@ -1,27 +1,23 @@
 import "reflect-metadata";
 
 import { APIGatewayProxyEvent } from "aws-lambda";
-import { plainToClass } from "class-transformer";
 
 import { BrowserService } from "../page-rendering/config/browser.service";
-import { CrawlUrl } from "./url-processing/crawl-url.model";
-import { UrlsProcessor } from "./url-processing/url-processor";
+import { crawlUrlStore } from "./url-processing/crawl-url.model";
 
 export class StartCrawlHandler {
   private bs = BrowserService;
   constructor(private event: APIGatewayProxyEvent) {}
 
   public async handle(): Promise<string> {
-    await this.bs.createBrowser();
-    await new UrlsProcessor(this.getUrl()).process();
-    await this.bs.close();
+    await crawlUrlStore.put({ url: this.getUrl(), level: 0 }).ifNotExists().exec();
     return "started";
   }
 
-  private getUrl(): CrawlUrl {
+  private getUrl(): string {
     if (this.event.body) {
       const { url } = JSON.parse(this.event.body);
-      return plainToClass(CrawlUrl, { url });
+      return url;
     }
     throw new Error(`Invalid body: ${this.event.body}`);
   }
