f2ee95512af3e4c2345285379357785ae14ea6bf
Ali Habibzadeh <alihabibzadeh@Alis-MacBook-Pro.local>
2020-05-28 13:55:54 +0100

    html in metrics

---
+++ src/metrics/metric-containers/html/page-html.ts
@@ -0,0 +1,33 @@
+import { Page, Response } from "puppeteer-core";
+
+import { Schema } from "@aws-cdk/aws-glue";
+
+import { BaseMetricContainer } from "../../base-types/base-metric-container";
+import { MetricNames } from "../../metrics-list";
+
+export class PageHtml extends BaseMetricContainer {
+  constructor(protected page: Page, response: Response | null) {
+    super(page, response);
+  }
+
+  public columns = [
+    { name: MetricNames.PreRenderDom, type: Schema.STRING, isGlueColumn: true },
+    { name: MetricNames.PostRenderDom, type: Schema.STRING, isGlueColumn: true }
+  ];
+
+  public async getMetrics(): Promise<Record<string, any>[]> {
+    return [
+      {
+        [this.columns[0].name]: await this.getOriginalSource(),
+        [this.columns[1].name]: await this.page.content()
+      }
+    ];
+  }
+
+  private async getOriginalSource(): Promise<string> {
+    if (this.response) {
+      return this.response.text();
+    }
+    return "page_didnt_respond";
+  }
+}
--- src/metrics/metrics-list.ts
+++ src/metrics/metrics-list.ts
@@ -4,16 +4,19 @@ import { InternalLinks } from "./metric-containers/internal-links/internal-links
 import { WebPerformance } from "./metric-containers/performance/performance";
 import { ResponseStatus } from "./metric-containers/status/status";
 import { PageUrl } from "./metric-containers/url/page-url";
+import { PageHtml } from "./metric-containers/html/page-html";
 
 export enum MetricNames {
   Url = "url",
   Status = "status",
   InternalLinks = "internal_links",
   FCP = "first_contentful_paint",
-  PageResourcesCount = "page_resources_count"
+  PageResourcesCount = "page_resources_count",
+  PreRenderDom = "pre_render_dom",
+  PostRenderDom = "post_redner_dom"
 }
 
-export const metricsContainers = [PageUrl, InternalLinks, ResponseStatus, WebPerformance];
+export const metricsContainers = [PageUrl, InternalLinks, ResponseStatus, WebPerformance, PageHtml];
 
 export function getGlueColumns(): Column[] {
   return metricsContainers
