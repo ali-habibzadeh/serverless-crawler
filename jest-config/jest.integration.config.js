e8e06931387fcee32db3546d9f71d77234eebd5e
Ali Habibzadeh <ali.hci@icloud.com>
2020-05-29 20:50:37 +0100

    test config

--- jest-config/jest.integration.config.js
+++ jest-config/jest.integration.config.js
@@ -1,4 +1,4 @@
-const output = require("./outputs.json");
+const output = require("../outputs.json");
 
 process.env = Object.assign(process.env, Object.values(output)[0]);
 
@@ -6,6 +6,6 @@ module.exports = {
   roots: ["<rootDir>/src"],
   testMatch: ["**/*.integration.ts"],
   transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
+    "^.+\\.ts$": "ts-jest"
+  }
 };
