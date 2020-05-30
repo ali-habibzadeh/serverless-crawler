08b0bab590727c796d8bdbead8d57bd4cfcab928
Ali Habibzadeh <ali.hci@icloud.com>
2020-05-30 12:29:45 +0100

    table permissions contained

--- src/@infrastructure/crawl-urls/crawl-urls-table.ts
+++ src/@infrastructure/crawl-urls/crawl-urls-table.ts
@@ -2,6 +2,7 @@ import { AttributeType, StreamViewType, Table } from "@aws-cdk/aws-dynamodb";
 import { StartingPosition } from "@aws-cdk/aws-lambda";
 import { DynamoEventSource } from "@aws-cdk/aws-lambda-event-sources";
 import { Construct, Duration, Tag } from "@aws-cdk/core";
+import { IGrantable } from "@aws-cdk/aws-iam";
 
 export class CrawlUrlsTable extends Construct {
   constructor(parent: Construct, id: string) {
@@ -23,6 +24,11 @@ export class CrawlUrlsTable extends Construct {
     retryAttempts: 4,
     batchSize: 40
   });
+
+  public grantNecessaryWrites(grantee: IGrantable): void {
+    this.table.grantWriteData(grantee);
+    this.table.grantStreamRead(grantee);
+  }
 
   private addTags(): void {
     Tag.add(this.table, "description", "Dynamodb table name for storing crawl URLs and trigerring crawling lambdas");
