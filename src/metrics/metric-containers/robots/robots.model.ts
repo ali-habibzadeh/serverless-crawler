bfd9d83e76279686b3a59566e3f670ddac44cf7a
Ali Habibzadeh <ali.hci@icloud.com>
2020-05-31 14:02:07 +0100

    robots

--- src/metrics/metric-containers/robots/robots.model.ts
+++ src/metrics/metric-containers/robots/robots.model.ts
@@ -6,23 +6,23 @@ export class RobotsTxt {
   private url = new URL(this.href);
   private folderLocation = `/tmp/${this.url.host}`;
   private robotsLocation = `${this.folderLocation}/robots.txt`;
-  constructor(private href: string) {
+  constructor(private href: string, private bot = "GoogleBot") {
     process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}`;
   }
 
   public async isAllowed(): Promise<boolean> {
-    if (!(await this.alreadyExists())) {
-      const robotstxt = await this.getRobotsFile();
+    if (!(await has(this.robotsLocation))) {
+      const robotstxt = await this.getRobots();
       if (!robotstxt) {
         return true;
       }
       await this.writeRobots(robotstxt);
     }
-    return this.getBinaryAllowedResults();
+    return this.getBinaryIsAllowed();
   }
 
-  private async getBinaryAllowedResults(): Promise<boolean> {
-    const { stdout } = await run(`robots ${this.robotsLocation} GoogleBot ${this.url.href}`);
+  private async getBinaryIsAllowed(): Promise<boolean> {
+    const { stdout } = await run(`robots ${this.robotsLocation} ${this.bot} ${this.url.href}`);
     return !stdout.includes("DISALLOWED");
   }
 
@@ -31,11 +31,7 @@ export class RobotsTxt {
     return write(this.robotsLocation, robotstxt);
   }
 
-  private async alreadyExists(): Promise<boolean> {
-    return has(this.robotsLocation);
-  }
-
-  private async getRobotsFile(): Promise<string | undefined> {
+  private async getRobots(): Promise<string | undefined> {
     try {
       const { data } = await axios.get(`${this.url.origin}/robots.txt`);
       return data;
