21b7a183f170a026a68a0c63d73e859e95e07ea4
Ali Habibzadeh <ali.hci@icloud.com>
2020-06-03 22:45:21 +0100

    try ws

--- src/page-rendering/config/browser.service.ts
+++ src/page-rendering/config/browser.service.ts
@@ -7,7 +7,7 @@ export class BrowserService {
   public static async createBrowser(): Promise<void> {
     if (this.browser) return;
     const { chromeClusterDns: dns, chromeClusterPort: port } = appConfig;
-    this.browser = await connect({ browserWSEndpoint: `http://${dns}:${port}` });
+    this.browser = await connect({ browserWSEndpoint: `ws://${dns}:${port}` });
   }
 
   public static getBrowser(): Browser {
