8612afcd2cbac7d3c06e1d7e83980b2a78ad173f
Ali Habibzadeh <ali.hci@icloud.com>
2020-06-05 22:38:54 +0100

    typo

--- src/utils/axios-cache.ts
+++ src/utils/axios-cache.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosAdapter } from "axios";
 import flatCache from "flat-cache";
 
-const axiosDisCache = flatCache.load("AxiosDiskCache", "/tmp/axios-cache");
+const axiosDiskCache = flatCache.load("AxiosDiskCache", "/tmp/axios-cache");
 
 interface ICachedResponse {
   data: any;
@@ -26,7 +26,7 @@ axios.interceptors.request.use(req => {
   const url = req.url;
   const shouldCache = req.headers.cache;
   if (req.method === "get" && url && shouldCache) {
-    const cachedResponse = <ICachedResponse>axiosDisCache.getKey(url);
+    const cachedResponse = <ICachedResponse>axiosDiskCache.getKey(url);
     if (cachedResponse) {
       req.adapter = getResponseAdapter(req, cachedResponse);
     }
@@ -38,12 +38,12 @@ axios.interceptors.response.use(res => {
   const shouldCache = res.config.headers.cache;
   const url = res.config.url;
   if (res.config.method === "get" && url && shouldCache) {
-    axiosDisCache.setKey(url, {
+    axiosDiskCache.setKey(url, {
       data: res.data,
       status: res.status,
       statusText: res.statusText
     });
-    axiosDisCache.save();
+    axiosDiskCache.save();
   }
   return res;
 });
