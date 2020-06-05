6ec64b2e124a8bd9c0e0cc0776b4dc69370f6c36
Ali Habibzadeh <ali.hci@icloud.com>
2020-06-05 11:30:46 +0100

    perf

--- src/metrics/metric-containers/performance/performance.ts
+++ src/metrics/metric-containers/performance/performance.ts
@@ -14,30 +14,17 @@ export class WebPerformance extends BaseMetricContainer {
   }
 
   public columns = [
-    { name: MetricNames.FirstContentfulPaint, type: Schema.INTEGER, isGlueColumn: true },
     { name: MetricNames.PageResourcesCount, type: Schema.INTEGER, isGlueColumn: true },
     { name: MetricNames.LargestContentfulPaint, type: Schema.INTEGER, isGlueColumn: true }
   ];
 
-  private extractPaintMetrics = () => {
-    const perf = performance.getEntries().filter(entry => entry.entryType === "paint");
-    return JSON.stringify(perf);
-  };
-
   public async getMetrics(): Promise<Record<string, number>[]> {
     return [
       {
-        [this.columns[0].name]: await this.getFirstContentfulPaint(),
         [this.columns[1].name]: await this.getResourceTreeCount(),
         [this.columns[2].name]: await this.getLargestContentfulPaint()
       }
     ];
-  }
-
-  private async getFirstContentfulPaint(): Promise<number> {
-    const paintEntries: PerformanceEntry[] = JSON.parse(await this.page.evaluate(this.extractPaintMetrics));
-    const fcp = paintEntries.find(entry => entry.name === "first-contentful-paint")?.startTime || 0;
-    return Math.round(fcp);
   }
 
   private async getResourceTreeCount(): Promise<number> {
