827412ac588a3288b57229c86489e8c1441764fc
Ali Habibzadeh <ali.hci@icloud.com>
2020-06-07 21:48:38 +0100

    cleaner

--- src/metrics/core-metrics/robots/robots-checker.ts
+++ src/metrics/core-metrics/robots/robots-checker.ts
@@ -11,11 +11,12 @@ export class RobotsChecker {
   private static instance: RobotsChecker;
   private static robotsMap = new Map<string, IWebsiteItem>();
 
-  private constructor() {}
+  private constructor() {
+    process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}`;
+  }
 
   public static getInstance(): RobotsChecker {
     if (!RobotsChecker.instance) {
-      process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}`;
       RobotsChecker.instance = new RobotsChecker();
     }
     return RobotsChecker.instance;
@@ -37,19 +38,15 @@ export class RobotsChecker {
   private static async addToMap(url: URL): Promise<void> {
     try {
       const { data } = await axios.get(`${url.origin}/robots.txt`);
-      await this.writeRobots(url, data);
+      await makeDir(`/tmp/${url.host}`, { recursive: true });
+      await write(`/tmp/${url.host}/robots.txt`, data);
       this.robotsMap.set(url.host, { hasRobots: true, alreadyStored: true });
     } catch {
       this.robotsMap.set(url.host, { hasRobots: false });
     }
   }
 
-  private static async writeRobots(url: URL, robotstxt: string): Promise<void> {
-    await makeDir(`/tmp/${url.host}`, { recursive: true });
-    return write(`/tmp/${url.host}/robots.txt`, robotstxt);
-  }
-
-  private static async getBinaryIsAllowed(url: URL, bot = "GoogleBot"): Promise<boolean> {
+  private static async getBinaryIsAllowed(url: URL, bot: string): Promise<boolean> {
     const { stdout } = await run(`robots /tmp/${url.host}/robots.txt ${bot} ${url.href}`);
     return !stdout.includes("DISALLOWED");
   }
