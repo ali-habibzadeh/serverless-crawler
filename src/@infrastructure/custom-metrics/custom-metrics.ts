a40029b6bd4f7b9d968c3e1ce99c6717bd95b908
Ali Habibzadeh <ali.hci@icloud.com>
2020-06-07 14:56:06 +0100

    don't deploy

--- src/@infrastructure/custom-metrics/custom-metrics.ts
+++ src/@infrastructure/custom-metrics/custom-metrics.ts
@@ -1,7 +1,6 @@
 import { Construct } from "@aws-cdk/core";
 import { AttributeType, StreamViewType, BillingMode, Table } from "@aws-cdk/aws-dynamodb";
 import { IGrantable } from "@aws-cdk/aws-iam";
-import { CfnCluster } from "@aws-cdk/aws-dax";
 
 export class CustomMetricsTable extends Construct {
   constructor(parent: Construct) {
@@ -14,12 +13,6 @@ export class CustomMetricsTable extends Construct {
     stream: StreamViewType.NEW_AND_OLD_IMAGES,
     timeToLiveAttribute: "ttl",
     billingMode: BillingMode.PAY_PER_REQUEST
-  });
-
-  public cluster = new CfnCluster(this, "CustomMetricsTableMemCache", {
-    iamRoleArn: "",
-    nodeType: "",
-    replicationFactor: 1
   });
 
   public grantAll(grantee: IGrantable): void {
