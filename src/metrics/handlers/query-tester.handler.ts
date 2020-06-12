10821461dec2cea3cc2dda3f172a2276aa1945cc
Ali Habibzadeh <ali.hci@icloud.com>
2020-06-12 15:31:44 +0100

    query

--- src/metrics/handlers/query-tester.handler.ts
+++ src/metrics/handlers/query-tester.handler.ts
@@ -1,12 +1,38 @@
 import { S3 } from "aws-sdk";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { appConfig } from "../../config/config.service";
+import { SelectObjectContentOutput, Object as S3Object } from "aws-sdk/clients/s3";
 
 export class QueryTesterHandler {
   private s3 = new S3();
   constructor(private event: APIGatewayProxyEvent) {}
 
-  public async handle(): Promise<any> {
-    return this.s3.listObjects({ Bucket: appConfig.crawlDataBucketName }).promise();
+  public async handle(): Promise<SelectObjectContentOutput[] | undefined> {
+    const { Contents } = await this.s3.listObjects({ Bucket: appConfig.crawlDataBucketName }).promise();
+    if (!Contents) {
+      return [];
+    }
+    return Promise.all(Contents.map(async object => this.queryObject(object)));
+  }
+
+  private queryObject(object: S3Object): Promise<SelectObjectContentOutput> {
+    return this.s3
+      .selectObjectContent({
+        Bucket: appConfig.crawlDataBucketName,
+        ExpressionType: "SQL",
+        Expression: this.getQuery(),
+        InputSerialization: {},
+        OutputSerialization: {},
+        Key: object.Key || ""
+      })
+      .promise();
+  }
+
+  private getQuery(): string {
+    if (this.event.body) {
+      const { query } = JSON.parse(this.event.body);
+      return query;
+    }
+    throw new Error(`Invalid body: ${this.event.body}`);
   }
 }
