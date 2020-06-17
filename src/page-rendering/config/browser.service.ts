49227a0ffe7febcdf0da8b40f1476da3088880c1
Ali Habibzadeh <ali.hci@icloud.com>
2020-06-17 20:02:03 +0100

    permissions

--- src/page-rendering/config/browser.service.ts
+++ src/page-rendering/config/browser.service.ts
@@ -1,21 +1,22 @@
 import { connect, Browser } from "puppeteer-core";
 import { appConfig } from "../../config/config.service";
+import { getChromiumArgs } from "./constants/chromium-switches";
 
 export class BrowserService {
-  private static browser?: Browser;
+  private static browser: Browser;
 
   public static async createBrowser(): Promise<void> {
     if (this.browser) return;
     const { chromeClusterDns: dns, chromeClusterPort: port } = appConfig;
-    this.browser = await connect({ browserWSEndpoint: `ws://${dns}:${port}` });
+    this.browser = await connect({ browserWSEndpoint: `ws://${dns}:${port}?${getChromiumArgs()}` });
   }
 
-  public static getBrowser(): Browser {
-    return this.browser!;
+  public static async getBrowser(url: string): Promise<Browser> {
+    await this.browser.defaultBrowserContext().overridePermissions(url, []);
+    return this.browser;
   }
 
   public static async close(): Promise<void> {
-    await this.browser!.close();
-    this.browser = undefined;
+    await this.browser.close();
   }
 }
